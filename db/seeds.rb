63b34d79b51e794ddfa200f9f1cdeae9af613fcd
Guilherme Cabral Vannuchi <guilhermevannuchi@gmail.com>
2018-01-29 20:07:25 -0200

    config: adding seeds and token secret

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+organization = Organization.find_or_create_by(name: 'Pipefy Recruitment Test',
+                      id: 92858,
+                      email: 'pipefydevrecruitingfakeuser@mailinator.com'
+                    )
